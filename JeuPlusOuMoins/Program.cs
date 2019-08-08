f8f73b4395c8bef0b8fb744999568a076e1bf8a8
jrousseau <jrousseau@DEV-JROUSSEAU>
2019-08-08 10:34:36 +0200

    add number of tries

--- JeuPlusOuMoins/Program.cs
+++ JeuPlusOuMoins/Program.cs
@@ -13,6 +13,8 @@ namespace JeuPlusOuMoins
 
             bool over = false;
 
+            int numberOfTries = 0;
+
             Console.WriteLine("Welcome to the Plus or minus game!");
             Console.WriteLine("Please enter a number and try to find the secret number (between 0 and 100)");
 
@@ -24,6 +26,8 @@ namespace JeuPlusOuMoins
 
                 if (int.TryParse(input, out inputToNumber))
                 {
+                    numberOfTries++;
+
                     if (inputToNumber < 0 || inputToNumber > 100)
                     {
                         Console.WriteLine("Please enter an integer between 0 and 100");
@@ -33,6 +37,7 @@ namespace JeuPlusOuMoins
                         Console.WriteLine("Success !");
                         Console.WriteLine("Number to be found : " + numberToBeFound);
                         Console.WriteLine("Your number : " + inputToNumber);
+                        Console.WriteLine("Tries needed : {0}", numberOfTries);
                         over = true;
                     }
                     else if (inputToNumber < numberToBeFound)
@@ -46,6 +51,8 @@ namespace JeuPlusOuMoins
                 }
                 else
                 {
+                    numberOfTries++;
+
                     Console.WriteLine("Please, only enter an integer");
                 }
             }
